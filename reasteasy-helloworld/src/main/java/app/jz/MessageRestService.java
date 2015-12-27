8266aa47bf7931819c452d6b9234efb75e335744
Wenjie Zhang <wenjiezhang2013@gmail.com>
2015-12-27 14:15:40 -0800

    Updated configuration

--- reasteasy-helloworld/src/main/java/app/jz/MessageRestService.java
+++ reasteasy-helloworld/src/main/java/app/jz/MessageRestService.java
@@ -3,9 +3,11 @@ package app.jz;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.Response;
 
 @Path("/message")
+@Produces("application/json")
 public class MessageRestService {
 
 	@GET
